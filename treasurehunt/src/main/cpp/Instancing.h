f72031b0065ae7fcb5acdd4949a14410ace67fed
playbar <hgl868@126.com>
2017-06-20 16:41:14 +0800

    添加程序代码

---
+++ treasurehunt/src/main/cpp/Instancing.h
@@ -0,0 +1,16 @@
+#ifndef __INSTANCING_H__
+#define __INSTANCING_H__
+
+#include "esUtil.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int InitMain ( ESContext *esContext );
+//int Init ( ESContext *esContext );
+
+#ifdef __cplusplus
+}
+#endif
+#endif
